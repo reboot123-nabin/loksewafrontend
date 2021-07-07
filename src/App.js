a2a35e51b8061e96c1eeab84a51654f75c447ebb
nabin <nabinmagar828@gmail.com>
2021-07-07 10:01:31 +0545

    first work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Registration from "./components/Registration";
+import {Route,Switch,dispath} from "react-router-dom";
+
+
 
 function App() {
+
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+  
+    <Route exact path="/registration">
+      <Registration></Registration>
+    </Route>
+    </>
   );
 }
 
+
+
 export default App;
+
